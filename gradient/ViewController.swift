249f7251ca45c72e4b54fb710a5544b27986cbb0
Daniel Washington <danielvertigo@hotmail.com>
2021-06-06 18:00:12 -0300

    Initial Commit

---
+++ gradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gradient
+//
+//  Created by Daniel Washington Ignacio on 06/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
